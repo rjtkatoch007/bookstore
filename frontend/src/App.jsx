480f9646da217dd773194194e36445b5fe0530da
rjtkatoch007 <rjtkatoch007@gmail.com>
2025-03-27 20:49:29 +0530

    React router v7 installed and configured.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3xl text-red-500 font-bold underline'>Jay Shree Ram!</h1>
+      <Outlet />
     </>
   )
 }
