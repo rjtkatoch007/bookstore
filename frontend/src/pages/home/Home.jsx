9c2f6a7b827017bb6701b63e24520c1eab1ff30e
rjtkatoch007 <rjtkatoch007@gmail.com>
2025-04-01 22:19:41 +0530

    Recommended and news section added to Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from './Banner'
 import TopSellers from './TopSellers'
+import Recommended from './Recommended'
+import News from './News'
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <TopSellers />
+            <Recommended />
+            <News />
         </div>
     )
 }
