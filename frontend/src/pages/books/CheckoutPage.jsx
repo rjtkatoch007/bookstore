107080358c65a57b62c7a3a772a13c9968dc8cf7
rjtkatoch007 <rjtkatoch007@gmail.com>
2025-04-04 16:46:41 +0530

    Checkout page issue resolved

--- frontend/src/pages/books/CheckoutPage.jsx
+++ frontend/src/pages/books/CheckoutPage.jsx
@@ -16,6 +16,7 @@ const CheckoutPage = () => {
     } = useForm()
 
     const onSubmit = (data) => {
+        //console.log(data);
         const newOrder = {
             name: data.name,
             email: currentUser?.email,
@@ -57,7 +58,7 @@ const CheckoutPage = () => {
                                         <div className="md:col-span-5">
                                             <label htmlFor="full_name">Full Name</label>
                                             <input
-
+                                                {...register("name", { required: true })}
                                                 type="text" name="name" id="name" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                                         </div>
 
@@ -70,24 +71,24 @@ const CheckoutPage = () => {
                                                 defaultValue={currentUser?.email}
                                                 placeholder="email@domain.com" />
                                         </div>
-                                        {/* <div className="md:col-span-5">
+                                        <div className="md:col-span-5">
                                             <label html="phone">Phone Number</label>
                                             <input
-
-                                                type="number" name="phone" id="phone" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="+123 456 7890" />
-                                        </div> */}
+                                                {...register("phone", { required: true })}
+                                                type="text" name="phone" id="phone" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="+123 456 7890" />
+                                        </div>
 
                                         <div className="md:col-span-3">
                                             <label htmlFor="address">Address / Street</label>
                                             <input
-
+                                                {...register("address", { required: true })}
                                                 type="text" name="address" id="address" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
                                         </div>
 
                                         <div className="md:col-span-2">
                                             <label htmlFor="city">City</label>
                                             <input
-
+                                                {...register("city", { required: true })}
                                                 type="text" name="city" id="city" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
                                         </div>
 
@@ -95,7 +96,7 @@ const CheckoutPage = () => {
                                             <label htmlFor="country">Country / region</label>
                                             <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                                                 <input
-
+                                                    {...register("country", { required: true })}
                                                     name="country" id="country" placeholder="Country" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
                                                 <button tabIndex="-1" className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
                                                     <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
@@ -113,7 +114,7 @@ const CheckoutPage = () => {
                                             <label htmlFor="state">State / province</label>
                                             <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                                                 <input
-
+                                                    {...register("state", { required: true })}
                                                     name="state" id="state" placeholder="State" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
                                                 <button className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
                                                     <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
@@ -130,7 +131,7 @@ const CheckoutPage = () => {
                                         <div className="md:col-span-1">
                                             <label htmlFor="zipcode">Zipcode</label>
                                             <input
-
+                                                {...register("zipcode", { required: true })}
                                                 type="text" name="zipcode" id="zipcode" className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
                                         </div>
 
